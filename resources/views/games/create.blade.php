481a814a393c6780750de5fd60568092047c29ee
saurabhbhoir456 <saurabhbhoir456@gmail.com>
2024-09-30 12:36:04 +0530

    tailwind cdn added

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create New Game</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 py-8">
